98111b589ce94db5f58eb65b1d53ca55c2f7dfea
Helen <helen7089@gmail.com>
2019-04-12 11:43:37 +0300

    register template

---
+++ client/src/views/Register.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs6 offset-xs3>
+        <h1>Register</h1>
+        <v-text-field
+        ></v-text-field>
+
+        <v-text-field
+        ></v-text-field>
+
+        <v-alert>
+        </v-alert>
+
+        <v-btn color="green" dark>
+          <v-icon class="mr-2">account_circle</v-icon>
+          Register
+        </v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
