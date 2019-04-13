7989c5ede828c14e2e038d8ebb792713324d4b92
Helen <helen7089@gmail.com>
2019-04-13 19:57:18 +0300

    Added content style

---
+++ client/src/components/Projects.vue
@@ -0,0 +1,30 @@
+<template>
+  <Panel title="Projects">
+    <v-layout row wrap>
+      <v-flex xs9>
+        <v-text-field
+          placeholder="Add List">
+        </v-text-field>
+      </v-flex>
+      <v-flex xs3>
+        <v-btn
+          dark
+          class="mt-2"
+          color="green">
+          <v-icon class="mr-2">add_circle</v-icon>
+          Add
+        </v-btn>
+      </v-flex>
+    </v-layout>
+  </Panel>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
---
+++ client/src/views/Projects.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <v-layout>
+      <v-flex xs4>
+        <Projects></Projects>
+      </v-flex>
+
+      <v-flex xs8>
+        <Panel title="Tasks">
+          <h1>Testing</h1>
+        </Panel>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import Projects from '@/components/Projects.vue';
+
+export default {
+  components: {
+    Projects,
+  },
+};
+</script>
