4b61005c3a0673c3cce46e67a87fa5f4a079d63b
Helen <helen7089@gmail.com>
2019-04-14 11:41:10 +0300

    Edit/add/delete project - works

--- client/src/components/Projects.vue
+++ client/src/components/Projects.vue
@@ -1,17 +1,53 @@
 <template>
   <Panel title="Projects">
     <div
+      class="project mt-2"
       v-for="project in projects"
       :key="project.id"
     >
-      {{project.title}}
+      <v-layout>
+        <v-flex xs9 class="text-xs-left">
+          <span
+            v-if="!project.isEditMode">
+            {{project.title}}
+          </span>
+          <v-text-field
+            autofocus
+            v-if="project.isEditMode"
+            :value="project.title"
+            @keyup.enter="saveProject(project)"
+            @input="setProjectTitle({
+              project,
+              title: $event,
+            })"
+          ></v-text-field>
+        </v-flex>
+        <v-flex xs3>
+          <v-icon
+            v-if="!project.isEditMode"
+            @click="setEditMode(project)">
+            edit
+          </v-icon>
+          <v-icon
+            v-if="project.isEditMode"
+            @click="saveProject(project)">
+            check
+          </v-icon>
+          <v-icon
+            class="trash"
+            @click="deleteProject(project)">
+            delete
+          </v-icon>
+        </v-flex>
+      </v-layout>
     </div>
-    <v-layout row wrap>
+    <v-layout row wrap class="mt-4">
       <v-flex xs8>
         <v-text-field
           placeholder="Add List"
           @input="setNewProjectName"
           :value="newProjectName"
+          @keyup.enter="createProject"
         ></v-text-field>
       </v-flex>
       <v-flex xs4>
@@ -32,6 +68,9 @@
 import { mapMutations, mapState, mapActions } from 'vuex';
 
 export default {
+  mounted() {
+    this.fetchProjects();
+  },
   computed: {
     ...mapState('projects', [
       'newProjectName',
@@ -41,14 +80,30 @@ export default {
   methods: {
     ...mapMutations('projects', [
       'setNewProjectName',
+      'setEditMode',
+      'setProjectTitle',
     ]),
     ...mapActions('projects', [
       'createProject',
+      'fetchProjects',
+      'saveProject',
+      'deleteProject',
     ]),
   },
 };
 </script>
 
 <style>
-
+.project {
+  font-size: 18px;
+}
+.icon {
+  cursor: pointer;
+}
+.v-icon:hover {
+  color: green;
+}
+.trash:hover {
+  color: red;
+}
 </style>
