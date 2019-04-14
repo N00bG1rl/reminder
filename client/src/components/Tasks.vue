67e1822d594343c9a83c89f3ba8a949af929613c
Helen <helen7089@gmail.com>
2019-04-14 22:15:07 +0300

    Tasks adding working

---
+++ client/src/components/Tasks.vue
@@ -0,0 +1,48 @@
+
+<template>
+  <Panel title="Tasks">
+    <div
+      class="tasks mt-2"
+      v-for="task in tasks"
+      :key="task.id"
+    >
+    {{task.description}}
+    </div>
+
+    <CreateRecord
+      placeholder="Add list"
+      @onInput="setNewTaskName"
+      :value="newTaskName"
+      @create="createTask"
+    />
+  </panel>
+</template>
+
+<script>
+import { mapState, mapMutations, mapActions } from 'vuex';
+import CreateRecord from '@/components/CreateRecord.vue';
+
+export default {
+  components: {
+    CreateRecord,
+  },
+  computed: {
+    ...mapState('tasks', [
+      'tasks',
+      'newTaskName',
+    ]),
+  },
+  methods: {
+    ...mapActions('tasks', [
+      'createTask',
+    ]),
+    ...mapMutations('tasks', [
+      'setNewTaskName',
+    ]),
+  },
+};
+</script>
+
+<style>
+
+</style>
---
+++ client/src/store/tasks.js
@@ -0,0 +1,42 @@
+
+// import router from '../router';
+/* import Vue from 'vue'; */
+import HTTP from '../http';
+
+export default {
+  namespaced: true,
+  state: {
+    tasks: [],
+    newTaskName: null,
+  },
+  actions: {
+    fetchTasksForProject({ commit }, project) {
+      return HTTP().get(`projects/${project.id}/tasks`)
+        .then(({ data }) => {
+          commit('setTasks', data);
+        });
+    },
+    createTask({ commit, state, rootState }) {
+      return HTTP().post(`/projects/${rootState.projects.currentProject.id}/tasks`, {
+        description: state.newTaskName,
+      })
+        .then(({ data }) => {
+          commit('appendTask', data);
+          commit('setNewTaskName', null);
+        });
+    },
+  },
+  getters: {
+  },
+  mutations: {
+    setTasks(state, tasks) {
+      state.tasks = tasks;
+    },
+    setNewTaskName(state, newTaskName) {
+      state.newTaskName = newTaskName;
+    },
+    appendTask(state, task) {
+      state.tasks.push(task);
+    },
+  },
+};
